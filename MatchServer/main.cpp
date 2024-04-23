9c7ef86bc28fd3a049a8abc42090a21ae0811bc4
1n$0mn14 <31206374+1nS0mni4@users.noreply.github.com>
2024-04-23 18:06:07 +0900

    init 2

---
+++ MatchServer/main.cpp
@@ -0,0 +1,7 @@
+#include "pch.h"
+
+int main(void) {
+	cout << "Test\n";
+
+	return 0;
+}
---
+++ ServerCore/CorePch.h
@@ -0,0 +1,20 @@
+#pragma once
+
+#include "pch.h"
+#include "Types.h"
+
+#include <vector>
+#include <list>
+#include <queue>
+#include <map>
+#include <set>
+#include <unordered_map>
+#include <unordered_set>
+
+#include <iostream>
+
+#include <Windows.h>
+#pragma comment(lib, "ws2_32.lib")
+#include <WinSock2.h>
+
+using namespace std;
